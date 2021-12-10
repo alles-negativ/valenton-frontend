422e0a9bc629716b42455faeaddbe13b6ac9d326
CarloNatter <61711997+GenepoolWoW@users.noreply.github.com>
2021-12-10 14:23:31 +0100

    [Add:] Reference component

---
+++ components/Referenzen/Referenzen.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="container"> 
+        <ul class="container__columns">
+            <li v-for="reference in references" :key="reference.id" class="content">
+                <nuxt-img :src="reference.images[0].url" :alt="reference.images[0].alt" />
+                <h3>{{ reference.title }}</h3>
+                <div class="text__big" v-html="reference.introtext"></div>
+                <div class="text" v-html="reference.contenttext"></div>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Referenzen',
+
+    data() {
+        return {
+            references: []
+        }
+    },
+    async fetch() {
+        const { json: data } = await this.$kirby.find({
+            "query": "page('referenzen').children",
+            "select": {
+                "title": true,
+                "introtext": true,
+                "contenttext": true,
+                "url": true,
+                "referenceimages": true,
+                "images": {
+                    "query": 'page.files',
+                    "select": {
+                        "name": true,
+                        "url": true,
+                        "alt": true
+                    }
+                }
+            }
+        })
+        this.references = data
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+    @use "Referenzen";
+</style>
--- pages/referenzen.vue
+++ pages/referenzen.vue
@@ -3,6 +3,7 @@
     <div class="grid">
       <h1>{{ page.content.contenttitle }}</h1>
       <div v-html="page.content.contenttext"></div>
+      <Referenzen />
     </div>
   </main>
 </template>
