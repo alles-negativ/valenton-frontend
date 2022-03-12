588688db43231719cf2e4c8b204ccc86e33fe0bc
Carlo <61711997+GenepoolWoW@users.noreply.github.com>
2022-03-12 16:52:50 +0100

    [Fix]: Arial Narrow Accordion / Title Reference

--- components/Anlistung/Anlistung.vue
+++ components/Anlistung/Anlistung.vue
@@ -1,15 +1,17 @@
 <template>
-    <div v-if="data != null">
+    <div class="content" v-if="data != null">
         <div class="container" v-for="element in JSON.parse(data.content.inhalt)" :key="element.id">
-            <h1 class="acc__title" v-if="element.type == 'heading'"> {{ element.content.text }}</h1>
-            <Accordion v-if="element.type == 'accordion'">
-                <template v-slot:title>
-                    <p class="text"> {{ element.content.title }} </p>
-                </template>
-                <template v-slot:body>
-                    <div class="text" slot="contenttext" v-html="element.content.text"></div>
-                </template>
-            </Accordion>
+            <div class="container__block">
+                <h1 class="acc__title" v-if="element.type == 'heading'"> {{ element.content.text }}</h1>
+            </div>     
+                <Accordion v-if="element.type == 'accordion'">
+                    <template v-slot:title>
+                        <p class="text"> {{ element.content.title }} </p>
+                    </template>
+                    <template v-slot:body>
+                        <div class="text" slot="contenttext" v-html="element.content.text"></div>
+                    </template>
+                </Accordion>
         </div>
     </div>
 </template>
